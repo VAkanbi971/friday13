cbe82676ffb16caba9fd361bad58c5268249bb87
Chris <ckim990@west-mec.org>
2020-03-13 15:19:01 -0700

    Comments

--- index.php
+++ index.php
@@ -17,11 +17,15 @@
   </style>
 </head>
 <body>
+    
+<!-- This is the title to our project. It is centered. -->
 
 <div class="jumbotron text-center" style="margin-bottom:0">
   <h1>Masks R Us</h1>
   <p></p> 
 </div>
+
+<!-- A simple nav bar with some placeholder links. -->
 
 <nav class="navbar navbar-expand-sm navbar-dark">
   <a class="navbar-brand" href="#">Navbar</a>
@@ -43,20 +47,23 @@
   </div>  
 </nav>
 
+<!-- This is a container with headers with varying sizes. -->
+
 <div class="container" style="margin-top:30px">
   <div class="row">
     <div class="col-sm-4">
       <h2>About The Company</h2>
       <h5>What We Do!</h5>
       <p>We sell masks to help people and for money.</p>
-
     </div>
+
+    <!-- This is a simple body text column. With a picture of a mask.-->
+
     <div class="col-sm-8" style="margin-top:30px">
       <h2>Masks</h2>
       <h5>We sell masks</h5>
       <img src="illustrain09-iryou5.png" alt="download">
       <p></p>
       <p>Due to the recent COVID-19 outbreak we are just pumping out masks for a fair price. These masks are next to impenetrable by any virus. We are here to make as many masks as humanly possible.</p>
-
 </body>
 </html>
